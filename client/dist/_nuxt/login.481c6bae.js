cc88ab6ab4a83b32b3fd97e37cb75d2eb9ff9317
Eden-Cohen1 <edencohen.dev@gmail.com>
2023-12-24 18:18:38 +0200

    npm run generate

--- client/dist/_nuxt/login.481c6bae.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as o,o as c,a as n}from"./entry.dccc383a.js";const s={},t=n("div",{class:"w-96 h-96 bg-blue-400 rounded-2xl"},null,-1),a=[t];function r(_,l){return c(),o("main",null,a)}const i=e(s,[["render",r]]);export{i as default};
